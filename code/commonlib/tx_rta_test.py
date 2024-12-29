04037af81886ee3140e38e6c1306b1bbada43a79
xiongyouguo <xiongyouguo@360.cn>
2024-12-29 19:33:30 +0800

    URLPost数据

--- code/commonlib/tx_rta_test.py
+++ code/commonlib/tx_rta_test.py
@@ -1,35 +1,56 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+#! /usr/bin/env python
+
 import hashlib
 from urllib import request, parse
+import json
+import time
 
 
-def getMD5(data):
+def getMD5(_data):
     m2 = hashlib.md5()
-    m2.update(data)
+    m2.update(_data.encode('utf-8'))
     str_md5 = m2.hexdigest()
-    return str_md5.upper()
+    return str_md5
 
 
-data_list = parse.urlencode(
-    {"data_list": [{
-        "game_session": "g1",
-        "timestamp": 123,
-        "strategy_id": "s1",
-        "exp_id": "e1",
+def url_param(_body, _token):
+    param = {}
+    ts = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+    param['time'] = ts
+    base = _body + _token + ts
+    sign_md5 = getMD5(base.encode("utf-8"))
+    param['authorization'] = sign_md5
+    return param
+
+
+if __name__ == '__main__':
+
+    ts = str(int(time.time()))
+    print(ts)
+    data = {"data_list": [{
+        "game_session": "CBgAA8JyfBuqS19Efq-n5tW4Vx3smqefolWeJj9REEsWhBo4vkc7n6Cns5jPYazmPOhgX-F_FfQuA",
+        "timestamp": 17353205320,
+        "strategy_id": "4",
+        "exp_id": "wx7f3cbe05a268e325",
         "ad_pos_type": 1,
         "act_type": 1,
         "count": 1
     }]}
-)
 
-req = request.Request('https://game.xxx.qq.com/cgi-bin/gamewxagpartnerrtaapi/game360rtafeedback')
-req.add_header('Origin', 'https://passport.weibo.cn')
-req.add_header('User-Agent', 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
-req.add_header('Referer', 'https://passport.weibo.cn/signin/login?entry=mweibo&res=wel&wm=3349&r=http%3A%2F%2Fm.weibo.cn%2F%3Fjumpfrom%3Dweibocom')
+    token = "360_rta_test"
 
-with request.urlopen(req, data=data_list.encode('utf-8')) as f:
-    print('Status:', f.status, f.reason)
-    for k, v in f.getheaders():
-        print('%s: %s' % (k, v))
-    print('Data:', f.read().decode('utf-8'))
+    try:
+        body = json.dumps(data)
+        print(body)
+        body_md5 = getMD5(json.dumps(body)).lower()
+        print(body_md5)
+
+        auth = getMD5(body_md5 + token + ts)
+        print(auth)
+
+        qq_url = "https://xxx/cgi-bin/gamewxagpartnerrtaapi/game360costfeedback?time={_ts}&authorization={_authorization}".format(_ts=ts, _authorization=auth)
+        print(qq_url)
+        res = request.urlopen(request.Request(qq_url, body.encode('utf-8'), {'Content-Type': 'application/json'}))
+        print(res.read())
+    except Exception as e:
+        print(Exception, ":", e)
